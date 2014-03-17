dfbe6923696a8fdc5cdc5b0c0e9fdbb7746adfcf
Kael Spencer <kael@euler>
2014-03-16 22:07:10 -0700

    Add enlister This window plugin looks for the project settings "enlister" and will launch provided commands via subprocess.

---
+++ enlister.py
@@ -0,0 +1,37 @@
+import sublime, sublime_plugin
+import subprocess
+import os
+
+class EnlisterCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.show_quick_panel()
+
+    def show_quick_panel(self):
+        pd = self.window.project_data();
+        if 'enlister' not in pd:
+            return
+
+        self.commands = pd['enlister']
+
+        panel_data = []
+        for cmd in self.commands:
+            try:
+                panel_data.append(cmd['name'])
+            except KeyError as e:
+                if str(e) == "'name'":
+                    print('enlister commands require "name". Provided (' + str(cmd) + ')')
+                    sublime.error_message('enlister commands require "name". Provided (' + str(cmd) + ')')
+                else:
+                    raise(e)
+
+        if not len(panel_data):
+            return
+
+        self.window.show_quick_panel(panel_data, self.on_quick_panel_select)
+
+    def on_quick_panel_select(self, picked):
+        if picked < 0:
+            return
+
+        print(self.commands[picked])
+        subprocess.Popen(self.commands[picked]['command'], cwd=self.commands[picked]['working_dir'], shell=True)
